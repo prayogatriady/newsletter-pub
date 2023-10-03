ac0033b7860d5d71a2fd964333e527134e4884b4
prayogatriady <prayogatriady.work@gmail.com>
2023-10-04 05:36:41 +0700

    Add ping service

--- http/router/router.go
+++ http/router/router.go
@@ -1,12 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func ApiRoutes(app *gin.RouterGroup) {
 	injectEmail := InitEmail()
+	injectPing := InitPing()
 
 	api := app.Group("/api")
 	{
+		api.GET("/ping", injectPing.Ping)
+
 		emailGroup := api.Group("/email")
 		{
 			emailGroup.POST("/sendEmail", injectEmail.SendEmail)

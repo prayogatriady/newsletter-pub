cfc88d8917e3876f56c8addc88fd6c5df36a3bf7
prayogatriady <prayogatriady.work@gmail.com>
2023-10-03 10:23:43 +0700

    Update lostinspace

--- api/v1/lostinspace/controller.go
+++ api/v1/lostinspace/controller.go
@@ -1,6 +1,7 @@
 package lost
 
 import (
+	"fmt"
 	"net/http"
 	"newsletter-pub/http/httpresponse"
 
@@ -9,10 +10,19 @@ import (
 
 func LostInSpace(c *gin.Context) {
 
+	var errorMessage string
+
+	lang := c.GetHeader("lang")
+	if lang == "EN" {
+		errorMessage = "You are lost in space"
+	} else if lang == "ID" {
+		errorMessage = "Anda tersesat"
+	}
+
 	httpresponse.BaseResponse(&httpresponse.HttpParams{
 		GinContext:   c,
 		StatusCode:   http.StatusOK,
 		ServiceName:  "LostInSpace",
-		ErrorMessage: "You are lost in space",
+		ErrorMessage: fmt.Sprintf("No Route: %s", errorMessage),
 	})
 }
